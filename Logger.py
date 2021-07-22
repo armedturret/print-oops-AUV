e3af142f730cf59804900cd0cc19742e8ae2eaca
Robert White <rjwhitenh@outlook.com>
2021-07-22 11:00:17 -0400

    Added basic backseat code

--- Logger.py
+++ Logger.py
@@ -2,16 +2,19 @@ import numpy as np
 import datetime
 
 class Logger():
-    def __init__(self):
+    def __init__(self, print_events=False):
         timestamp = str(np.datetime64(datetime.datetime.now())).replace(':','')
         self.__file = open(f"logs/log-{timestamp}.txt", "a")
         self.__npfile = open(f"logs/data-{timestamp}.out", "a")
         self.__start = np.datetime64(datetime.datetime.now()) 
+        self.__print_events = print_events
 
     #logs an event in format timestamp [source]: message
     def log_event(self, source, message):
         timestamp = str(np.datetime64(datetime.datetime.now()))
         formatted = f"[{timestamp}] [{source}]: {message}\n"
+        if self.__print_events:
+            print(formatted)
         self.__file.write(formatted)
 
     def log_auv_location(self, x, y, heading):
