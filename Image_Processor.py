0bc7ebc7f338a1b8165dc9ecfc8f18d0c4acefaf
paeb <brandon_pae@horacemann.org>
2021-07-26 11:48:05 -0400

    oof

--- Image_Processor.py
+++ Image_Processor.py
@@ -138,7 +138,7 @@ class ImageProcessor():
                     self.__camera.capture(self.__image, 'bgr')
                 except:
                     # restart the camera
-                    self.__camera = picamera.PiCamera()
+                    #self.__camera = picamera.PiCamera()
                     self.__camera.resolution = (640, 480)
                     self.__camera.framerate = 24
                     time.sleep(2) # camera warmup time
